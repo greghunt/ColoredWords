77cb156920893a227885bf206e688598799767dd
Greg Hunt <freshbrewedweb@gmail.com>
2016-12-02 15:36:02 -0500

    adjusted example stlyes

--- example.php
+++ example.php
@@ -13,36 +13,46 @@ try {
   $error = $e->getMessage();
 }
 
-
 ?>
-<style>
-  input {
-    padding: 0.5em 1em;
-  }
-  button {
-    background: #aaa;
-    color: white;
-    border: none;
-  }
-</style>
-<form style="width:90%; max-width:400px;margin:3em auto;">
-  <div style="display:flex;">
-    <input type="text" name="word" value="<?= $word ?>" style="flex: 1 0 auto">
-    <button type="submit" style="flex: 1 0 auto">Convert</button>
-  </div>
-  <?php if( isset($error) ): ?>
-    <input
-      type="text"
-      value="<?= $error ?>"
-      style="width: 100%; text-align: center; margin-top: 1em; border:3px solid; border-bottom-width:5em;border-color:red;color: red"
-      disabled
-    >
-  <?php else: ?>
-  <input
-    type="text"
-    value="<?= $word ? $converted->name(TRUE) : "Not set" ?>"
-    style="width: 100%; text-align: center; margin-top: 1em; border:3px solid; border-bottom-width:5em;<?= $word ? 'border-color: ' . $converted->hex() : NULL ?>;<?= $word ? 'color: ' . $converted->hex() : NULL ?>"
-    disabled
-  >
-  <?php endif; ?>
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        background-color: <?= $word ? $converted->hex() : 'white' ?>;
+      }
+      input {
+        padding: 0.5em 1em;
+        box-sizing: border-box;
+      }
+      button {
+        background: black;
+        color: white;
+        border: none;
+      }
+    </style>
+  </head>
+  <body>
+    <form style="width:90%; max-width:400px;margin:3em auto;">
+      <div style="display:flex;">
+        <input type="text" name="word" value="<?= $word ?>" style="flex: 1 0 auto">
+        <button type="submit" style="flex: 1 0 auto">Convert</button>
+      </div>
+      <?php if( isset($error) ): ?>
+        <input
+          type="text"
+          value="<?= $error ?>"
+          style="width: 100%; text-align: center; margin-top: 1em; border:3px solid; border-bottom-width:5em;border-color:red;color: red"
+          disabled
+        >
+      <?php else: ?>
+      <input
+        type="text"
+        value="<?= $word ? $converted->name(TRUE) : "Not set" ?>"
+        style="width: 100%; text-align: center; margin-top: 1em;<?= $word ? 'color: ' . $converted->hex() : NULL ?>"
+        disabled
+      >
+      <?php endif; ?>
+    </form>
+  </body>
+</html>
